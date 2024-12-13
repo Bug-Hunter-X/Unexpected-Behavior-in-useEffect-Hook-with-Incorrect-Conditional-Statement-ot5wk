9f4105094e254a50905e66fc7d7f5a37f9c7028a
BugHunter-X <askzenai@gmail.com>
2024-12-13 05:00:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect condition
+    if (count > 0) {
+      console.log('Count is greater than 0');
+    }
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+```
